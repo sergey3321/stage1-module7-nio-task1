c96c28db2e252c4da8cb43124f3d1bef603e7a1e
sergey3321 <mail.sergey21@gmail.com>
2023-08-17 19:17:34 +0300

    stage1-module7-nio-task1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
+    String line = null;
+    Map<String, String> result = new HashMap<>();
+    String[] array = {};
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file))))
+        {
+            while ((line = reader.readLine()) != null) {
+                array = line.split(":");
+                result.put(array[0], array[1]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(result.get("Name").trim(), Integer.parseInt(result.get("Age").trim()), result.get("Email").trim(), Long.parseLong(result.get("Phone").trim()));
     }
 }
